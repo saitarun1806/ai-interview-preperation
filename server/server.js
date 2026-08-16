f4345bd0fd572d1d873152a9cedad471087bf0c8
kunani1 <kunanisaitarun55@gmail.com>
2026-08-16 10:17:11 +0530

    fix1

--- server/server.js
+++ server/server.js
@@ -22,7 +22,22 @@ const app = express();
 
 connectDB();
 
+const allowedOrigins = [
+  "https://ai-interview-preperation-six.vercel.app",
+  "http://localhost:5173",
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+   
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 
