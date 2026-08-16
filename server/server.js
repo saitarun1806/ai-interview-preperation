9b0303fb7af200161f8f5d908560cf521050a6f6
kunani1 <kunanisaitarun55@gmail.com>
2026-08-16 09:07:02 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
         credentials: true,
     })
 );
