3bded242233abbc75f5df233e77ea7f5014befb6
kunani1 <kunanisaitarun55@gmail.com>
2026-08-16 10:09:16 +0530

    fix1

--- server/server.js
+++ server/server.js
@@ -31,9 +31,10 @@ app.use(cors({
     "http://localhost:5173",
     "https://ai-interview-preperation-six.vercel.app"
   ],
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
 
 
 
